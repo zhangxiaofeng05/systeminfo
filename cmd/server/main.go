ef0a5b7007aa70b433a2c8cbea7bbf6c09dc6f7a
zhangxiaofeng05 <zhangxiaofeng05@foxmail.com>
2022-11-09 22:03:01 +0800

    graceful shutdown

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,11 +1,15 @@
 package main
 
 import (
+	"context"
 	"flag"
 	"fmt"
 	"log"
 	"net/http"
+	"os/signal"
 	"runtime"
+	"syscall"
+	"time"
 
 	"github.com/gin-contrib/pprof"
 	"github.com/gin-gonic/gin"
@@ -15,15 +19,18 @@ import (
 )
 
 var (
+	Port int // server port
+
 	Pprof bool // pprof gin middleware
 )
 
 func init() {
+	flag.IntVar(&Port, "port", 8080, "server port")
 	flag.BoolVar(&Pprof, "pprof", false, "use pprof gin middleware")
 
 	// customize -h param
 	flag.Usage = func() {
-		fmt.Println("Usage: systeminfo [-h] [-pprof bool]")
+		fmt.Println("Usage: systeminfo [-h] [-port int] [-pprof bool]")
 		flag.PrintDefaults()
 	}
 
@@ -32,9 +39,14 @@ func init() {
 }
 
 func main() {
+	// Create context that listens for the interrupt signal from the OS.
+	ctx, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
+	defer stop()
+
 	r := gin.Default()
 	if Pprof {
 		pprof.Register(r)
+		log.Printf("get pprof info at http://127.0.0.1:%d/debug/pprof", Port)
 	}
 
 	// ping
@@ -45,9 +57,38 @@ func main() {
 	// system info
 	r.GET("/system", getSystemInfo)
 
-	log.Println("get system info at http://127.0.0.1:8080/system")
+	log.Printf("get system info at http://127.0.0.1:%d/system", Port)
 
-	log.Fatal(r.Run())
+	srv := &http.Server{
+		Addr:    fmt.Sprintf(":%d", Port),
+		Handler: r,
+	}
+
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Listen for the interrupt signal.
+	<-ctx.Done()
+
+	// Restore default behavior on the interrupt signal and notify user of shutdown.
+	stop()
+	log.Println("shutting down gracefully, press Ctrl+C again to force")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown: ", err)
+	}
+
+	log.Println("Server exiting")
+
 }
 
 func getSystemInfo(c *gin.Context) {
